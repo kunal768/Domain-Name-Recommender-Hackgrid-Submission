42d5c9c1ecc319e1812e1e5babd7e5bfbb4a81ab
kunal768 <33108756+kunal768@users.noreply.github.com>
2019-03-12 16:08:09 +0530

    Update synonyms.py

--- HackGrid19/scripts/synonyms.py
+++ HackGrid19/scripts/synonyms.py
@@ -3,7 +3,7 @@ from nltk.corpus import wordnet
 
 def get_synonyms(word):
     synonyms = []
-    for syn in wordnet.synsets("good"):
+    for syn in wordnet.synsets(word):
         for l in syn.lemmas():
             synonyms.append(l.name())
     return synonyms 
